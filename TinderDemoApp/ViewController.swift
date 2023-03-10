0a88ffcd312b2341fe75e9ab9f64c819ba96fc7c
Rio-iOS <rio.2014.swift.ios@gmail.com>
2023-03-10 10:56:36 +0900

    ep1終了

---
+++ TinderDemoApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TinderDemoApp
+//
+//  Created by 藤門莉生 on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        view.backgroundColor = .white
+        
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        let view3 = UIView()
+        view3.backgroundColor = .green
+        
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [
+            view1.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 100),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ].forEach { $0.isActive = true }
+        
+    }
+
+
+}
+
---
+++ TinderDemoAppUITests/TinderDemoAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TinderDemoAppUITestsLaunchTests.swift
+//  TinderDemoAppUITests
+//
+//  Created by 藤門莉生 on 2023/03/10.
+//
+
+import XCTest
+
+final class TinderDemoAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
